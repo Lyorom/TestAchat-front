ecfd2433ed387eaebd9607356db87731b962f339
quet-romain <quet.romain@gmail.com>
2016-12-12 17:17:15 +0100

    js compter

---
+++ js/index.js
@@ -0,0 +1,38 @@
+//ticket jaune
+var numbreY = 0;
+
+function addY() {
+    numbreY++;
+    document.getElementById('numY').innerHTML = numbreY;
+}
+
+function removeY() {
+    numbreY--;
+    document.getElementById('numY').innerHTML = numbreY;
+}
+
+//ticket vert
+var numbreVE = 0;
+
+function addVE() {
+    numbreVE++;
+    document.getElementById('numVE').innerHTML = numbreVE;
+}
+
+function removeVE() {
+    numbreVE--;
+    document.getElementById('numVE').innerHTML = numbreVE;
+}
+
+//ticket violet
+var numbreVI = 0;
+
+function addVI() {
+    numbreVI++;
+    document.getElementById('numVI').innerHTML = numbreVI;
+}
+
+function removeVI() {
+    numbreVI--;
+    document.getElementById('numVI').innerHTML = numbreVI;
+}
