b53975dfcaf1bcec2c6440484245ffb7f743cad1
quet-romain <quet.romain@gmail.com>
2016-12-13 11:16:14 +0100

    add panier modif position

--- js/index.js
+++ js/index.js
@@ -1,38 +1,71 @@
+//ACHAT//
+
 //ticket jaune
-var numbreY = 0;
+var numbre1 = 0;
 
-function addY() {
-    numbreY++;
-    document.getElementById('numY').innerHTML = numbreY;
+function add1() {
+    numbre1++;
+    document.getElementById('num1').innerHTML = numbre1;
 }
-
-function removeY() {
-    numbreY--;
-    document.getElementById('numY').innerHTML = numbreY;
+function remove1() {
+    if (numbre1<1) {
+        numbre1 = 0;
+    }
+    else {
+        numbre1--;
+        document.getElementById('num1').innerHTML = numbre1;
+    }
 }
 
 //ticket vert
-var numbreVE = 0;
+var numbre2 = 0;
 
-function addVE() {
-    numbreVE++;
-    document.getElementById('numVE').innerHTML = numbreVE;
+function add2() {
+    numbre2++;
+    document.getElementById('num2').innerHTML = numbre2;
 }
-
-function removeVE() {
-    numbreVE--;
-    document.getElementById('numVE').innerHTML = numbreVE;
+function remove2() {
+    if (numbre2<1) {
+        numbre2 = 0;
+    }
+    else {
+        numbre2--;
+        document.getElementById('num2').innerHTML = numbre2;
+    }
 }
 
 //ticket violet
-var numbreVI = 0;
+var numbre3 = 0;
 
-function addVI() {
-    numbreVI++;
-    document.getElementById('numVI').innerHTML = numbreVI;
+function add3() {
+    numbre3++;
+    document.getElementById('num3').innerHTML = numbre3;
 }
 
-function removeVI() {
-    numbreVI--;
-    document.getElementById('numVI').innerHTML = numbreVI;
+function remove3() {
+    if (numbre3<1) {
+        numbre3 = 0;
+    }
+    else {
+        numbre3--;
+        document.getElementById('num3').innerHTML = numbre3;
+    }
 }
+
+//ENREGISTRE
+function enregistre() {
+    localStorage.setItem('jaune',numbre1);
+    localStorage.setItem('vert',numbre2);
+    localStorage.setItem('violet',numbre3);
+}
+function panier() {
+    var jaune = localStorage.getItem('jaune');
+    var vert = localStorage.getItem('vert');
+    var violet = localStorage.getItem('violet');
+
+    document.getElementById('jaune').innerHTML = jaune + ' ticket jaune dans le panier';
+    document.getElementById('vert').innerHTML = vert + ' ticket vert dans le panier';
+    document.getElementById('violet').innerHTML = violet + ' ticket violet dans le panier';
+
+}
+
